42ec9b54910663109b354b28240d8d7b84795138
Heitor <heitorbrandolin@hotmail.com>
2023-07-26 12:33:07 -0300

    Adicionando dados de autenticação Header

--- app/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
@@ -1,6 +1,9 @@
 package com.devmasterteam.tasks.service.repository.remote
 
+import com.devmasterteam.tasks.service.constants.TaskConstants
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,9 +12,23 @@ class RetrofitClient private constructor() {
 
         private lateinit var INSTANCE: Retrofit
         private const val BASE_URL = "http://devmasterteam.com/CursoAndroidAPI/"
+        private var token: String = ""
+        private var personKey: String = ""
 
         private fun getRetrofitInstance(): Retrofit {
             val httpClient = OkHttpClient.Builder()
+
+            httpClient.addInterceptor(object : Interceptor {
+                override fun intercept(chain: Interceptor.Chain): Response {
+                    val request = chain.request()
+                        .newBuilder()
+                        .addHeader(TaskConstants.HEADER.TOKEN_KEY, token)
+                        .addHeader(TaskConstants.HEADER.PERSON_KEY, personKey)
+                        .build()
+                    return chain.proceed(request)
+                }
+
+            })
 
             if (!::INSTANCE.isInitialized) {
                 synchronized(RetrofitClient::class) {
@@ -25,8 +42,13 @@ class RetrofitClient private constructor() {
             return INSTANCE
         }
 
-        fun <T> getService(serviceClass: Class<T>): T{
+        fun <T> getService(serviceClass: Class<T>): T {
             return getRetrofitInstance().create(serviceClass)
+        }
+
+        fun addHeaders(tokenValue: String, personKeyValue: String){
+            token = tokenValue
+            personKey = personKeyValue
         }
 
     }
