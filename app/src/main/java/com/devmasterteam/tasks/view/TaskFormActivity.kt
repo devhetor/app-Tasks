bc989d170f238ccdc98bbbb2e77a5d26d6ef4f45
Heitor <heitorbrandolin@hotmail.com>
2023-07-27 11:34:30 -0300

    cadastro de tarefas - Spinner

--- app/src/main/java/com/devmasterteam/tasks/view/TaskFormActivity.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/TaskFormActivity.kt
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.DatePicker
 import androidx.lifecycle.ViewModelProvider
 import com.devmasterteam.tasks.R
@@ -32,6 +33,15 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener, DatePickerDi
         binding.buttonSave.setOnClickListener(this)
         binding.buttonDate.setOnClickListener(this)
 
+        viewModel.loadPriorities()
+
+        val list = arrayOf("a", "b")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, list)
+        binding.spinnerPriority.adapter = adapter
+
+        observe()
+
+
         // Layout
         setContentView(binding.root)
     }
@@ -48,6 +58,17 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener, DatePickerDi
 
         val dueDate = dateFormat.format(calendar.time)
         binding.buttonDate.text = dueDate
+    }
+
+    private fun observe(){
+        viewModel.priorityList.observe(this){
+            val list = mutableListOf<String>()
+            for(p in it){
+                list.add(p.description)
+            }
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, list)
+            binding.spinnerPriority.adapter = adapter
+        }
     }
 
     private fun handleDate(){
