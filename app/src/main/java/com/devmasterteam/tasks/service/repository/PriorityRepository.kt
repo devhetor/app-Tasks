9846eb9f08a7f8a7dcb2f3789a5c3f0aba148209
Heitor <heitorbrandolin@hotmail.com>
2023-07-27 12:23:16 -0300

    Refatoração de repositórios utilizando Generics

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
@@ -13,7 +13,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PriorityRepository(val context: Context) {
+class PriorityRepository(val context: Context) : BaseRepository() {
 
     private val remote = RetrofitClient.getService(PriorityService::class.java)
     private val database = TaskDatabase.getDatabase(context).priorityDAO()
@@ -22,15 +22,9 @@ class PriorityRepository(val context: Context) {
         val call = remote.list()
         call.enqueue(object : Callback<List<PriorityModel>> {
             override fun onResponse(
-                call: Call<List<PriorityModel>>,
-                response: Response<List<PriorityModel>>
+                call: Call<List<PriorityModel>>, response: Response<List<PriorityModel>>
             ) {
-                if (response.code() == TaskConstants.HTTP.SUCCESS) {
-                    response.body()?.let { listener.onSucess(it) }
-                } else {
-
-                    listener.onFailure(failResponse(response.errorBody()!!.string()))
-                }
+                handleResponse(response, listener)
             }
 
             override fun onFailure(call: Call<List<PriorityModel>>, t: Throwable) {
@@ -40,17 +34,12 @@ class PriorityRepository(val context: Context) {
         })
     }
 
-    fun list(): List<PriorityModel>{
+    fun list(): List<PriorityModel> {
         return database.list()
     }
 
-    fun save(list: List<PriorityModel>){
+    fun save(list: List<PriorityModel>) {
         database.clear()
         database.save(list)
     }
-
-    private fun failResponse(str: String): String {
-        return Gson().fromJson(str, String::class.java)
-    }
-
 }
